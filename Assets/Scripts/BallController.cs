3732c8867a38caa7badfe97440b59790461dd17d
elijahchen <code.elijahchen@gmail.com>
2017-01-22 23:58:24 -0500

    - Added logic for win condition

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,12 +5,14 @@ public class BallController : MonoBehaviour {
 
     [SerializeField]
     private float speed;
-
+    bool started;
+    bool gameOver;
     private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-        rb.velocity = new Vector3(speed, 0, 0);
+        gameOver = false;
+        started = false;
 	}
 
     void Awake()
@@ -20,7 +22,20 @@ public class BallController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
+        if (!started)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+                started = true;
+            }
+        }
+        if(!Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            gameOver = true;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !gameOver)
         {
             SwitchDirection();
         }
